4d1b627e279ccb5f322b9aa62a9082f9614b89d0
papula-a <pa20381@auis.edu.krd>
2024-04-10 00:03:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let thankYouCards = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouCards;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+const names = ["Charlie", "Samip", "Ali"];
+const eventName = "birthday";
+const thankYouCards = writeCards(names, eventName);
+console.log(thankYouCards);
+countDown(10);
